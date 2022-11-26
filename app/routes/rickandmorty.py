3ada480220728ce7e67a9c7b730a8f0e859450cc
LuisHCh <huanca.luis@pucp.pe>
2022-11-25 20:45:52 -0500

    agregar actualizar

--- app/routes/rickandmorty.py
+++ app/routes/rickandmorty.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, flash, redirect
+from flask import Blueprint, render_template, url_for, flash, redirect, request
 from app.forms import RickandmortyForm
 from app.db import db
 from app.models.rickandmorty import RickandMorty
@@ -29,14 +29,14 @@ def obtenerPersonajes():
     personajesDB = db.rickandmorty.find()
     return render_template("indexRM.html",personajes=personajesDB)
 
-
+# Detalle del personaje
 @rickandmorty_router.route("/detail/<id>",methods=['GET','POST'])
 def detail_personaje(id):
     personaje = db.rickandmorty.find({"id": int(id)})
     personaje_detail = list(personaje)
     return render_template("detailRM.html",personaje=personaje_detail[0])
 
-
+# Eliminar personaje
 @rickandmorty_router.route("/eliminar/<id>")
 def delete_personaje(id):
   db.rickandmorty.delete_one({"name": id})
@@ -44,3 +44,22 @@ def delete_personaje(id):
   flash("Personaje eliminado", "success")
 
   return redirect(url_for('rickandmorty_router.index'))
+
+# Actualizar personaje
+@rickandmorty_router.route('/actualizar/<int:id>', methods=['GET', 'POST'])
+def vista_actualizar(id):
+    personaje = db.rickandmorty.find({"id": int(id)})
+    personaje_detail = list(personaje)
+    print(personaje_detail)
+
+    if request.method == 'POST':
+        updated_personaje = {
+            'name': request.form["name"], 
+            'status': request.form["status"],
+            'species': request.form["species"] 
+            , 'location.name': request.form["location"]
+            }
+        db.rickandmorty.update_one({'_id': personaje_detail[0]["_id"]}, {"$set": updated_personaje})
+        return redirect(url_for('rickandmorty_router.index'))
+
+    return render_template("actualizarPersonaje.html", personaje=personaje_detail[0])
